a8263bb2d71a1a3c0e330a2843aecb3d1da6c9ff
André Pinto <aempinto02@gmail.com>
2022-08-11 09:53:23 -0300

    Improves on Reactive service and controller

--- microservice/src/main/java/com/ston/spring/webflux/microservice/service/ReactiveMathService.java
+++ microservice/src/main/java/com/ston/spring/webflux/microservice/service/ReactiveMathService.java
@@ -1,5 +1,9 @@
 package com.ston.spring.webflux.microservice.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.springframework.stereotype.Service;
 
 import com.ston.spring.webflux.microservice.dto.Response;
@@ -15,8 +19,16 @@ public class ReactiveMathService {
 	}
 
 	public Flux<Response> multiplicationTable(int input) {
-		return Flux.range(1, 10).doOnNext(i -> SleepUtil.sleepSeconds(1))
-				.doOnNext(i -> System.out.println("react-math-service processing: " + i))
-				.map(i -> new Response(i * input));
+
+		List<Response> response = IntStream.rangeClosed(1, 10).peek(i -> SleepUtil.sleepSeconds(1))
+				.peek(i -> System.out.println("math-service processing: " + i)).mapToObj(i -> new Response(i * input))
+				.collect(Collectors.toList());
+
+		return Flux.fromIterable(response);
+
+//		return Flux.range(1, 10).delayElements(Duration.ofSeconds(1))
+//				.doOnNext(i -> SleepUtil.sleepSeconds(1))
+//				.doOnNext(i -> System.out.println("react-math-service processing: " + i))
+//				.map(i -> new Response(i * input));
 	}
 }
